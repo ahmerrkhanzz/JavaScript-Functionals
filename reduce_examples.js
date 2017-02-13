7ddf6bc87869a45f659188efdddd2d995b9065f2
αнмєяя кнαиzz <ahmerrkhanzz@gmail.com>
2017-02-13 13:07:27 +0500

    reduce another example added

--- reduce_examples.js
+++ reduce_examples.js
@@ -1,11 +1,19 @@
-JS Reduce method examples
+//JS Reduce method examples
 var arr = [
     {name: "ahmer", age: "25", occupation: "js dev", email: "ahmerrkhanzz@gmail.com"},
 	{name: "saeed", age: "26", occupation: "php dev", email: "ahmerrkhanzz@yahoo.com"},
 	{name: "khan", age: "27", occupation: "nodejs dev", email: "ahmerrkhanzz@outlook.com"}
 ];
+
+var ageSum = arr.reduce(function(all, item, index){
+	all =+ item.age;
+	return all;
+},  0);
+console.log("this will return the combined sum of all ages in the array.");
+console.log(ageSum);
 var e = arr.reduce(function(all, item, index) {
 	all[item.email] = item.occupation;
 	return all;
 }, {});
+console.log("this will return the new object with the given keys and values");
 console.log(e);

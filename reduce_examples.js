0de8a8e472e488820dfe7e782a00d7e49b4b43a0
αнмєяя кнαиzz <ahmerrkhanzz@gmail.com>
2017-02-13 13:12:34 +0500

    new reduce example added with if condition

--- reduce_examples.js
+++ reduce_examples.js
@@ -1,19 +1,33 @@
 //JS Reduce method examples
 var arr = [
-    {name: "ahmer", age: "25", occupation: "js dev", email: "ahmerrkhanzz@gmail.com"},
-	{name: "saeed", age: "26", occupation: "php dev", email: "ahmerrkhanzz@yahoo.com"},
-	{name: "khan", age: "27", occupation: "nodejs dev", email: "ahmerrkhanzz@outlook.com"}
+    	{name: "ahmer", gender: "male", age: "25", occupation: "js dev", email: "ahmerrkhanzz@gmail.com"},
+	{name: "saeed", gender: "male", age: "26", occupation: "php dev", email: "ahmerrkhanzz@yahoo.com"},
+	{name: "eliena", gender: "female", age: "27", occupation: "nodejs dev", email: "eliena@outlook.com"},
+	{name: "emma", gender: "female", age: "27", occupation: "nodejs dev", email: "emma@outlook.com"}
 ];
 
+//simple reduce method for adding ages
 var ageSum = arr.reduce(function(all, item, index){
 	all =+ item.age;
 	return all;
 },  0);
 console.log("this will return the combined sum of all ages in the array.");
 console.log(ageSum);
+
+//reduce method for creating new object from our array
 var e = arr.reduce(function(all, item, index) {
-	all[item.email] = item.occupation;
+	all[item.email] = item.name;
 	return all;
 }, {});
 console.log("this will return the new object with the given keys and values");
 console.log(e);
+
+//creating new object with conditions with the given array
+var x = arr.reduce(function(all, item, index) {
+	if(item.gender === "female"){
+		all[item.email] = item.name;
+	}
+	return all;
+}, {});
+console.log("this will return the new object with the given keys and values");
+console.log(x);
